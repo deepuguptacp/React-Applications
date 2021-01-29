69086a78e471cb88878fbe577de3206d530bf118
deepuguptacp <199deepu@gmail.com>
2021-01-29 13:55:36 +0530

    Counter App

---
+++ counter-app/src/components/counter.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatvalue()}</span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-success btn-sm m-3"
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          disabled={this.props.counter.value === 0 ? "disabled" : ""}
+          className="btn btn-success btn-sm m-3"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-3"
+        >
+          Delete
+        </button>
+        {/* <ul>{this.state.tags.map(tag => <li key = {tag}>{tag}</li>)}</ul> */}
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-3 badge-";
+    // classes += (this.state.value === 0) ? "warning" : "primary";
+    if (this.props.counter.value === 0) {
+      classes += "warning";
+    }
+    if (this.props.counter.value > 0) {
+      classes += "primary";
+    }
+    return classes;
+  }
+  formatvalue() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
