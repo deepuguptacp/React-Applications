67cf4281962065bf11257dff7cd104466a120079
deepuguptacp <199deepu@gmail.com>
2021-02-03 18:34:19 +0530

    Accordion

---
+++ accordion/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import AboutUs from "./content/personal-info";
+import Address from "./content/userAddress";
+
+const data = [
+  {
+    id: 1,
+    name: "Personal Information",
+    content: () => <AboutUs />,
+    state: "inactive",
+  },
+  {
+    id: 2,
+    name: "Permanent Address",
+    content: () => <Address />,
+    state: "inactive",
+  },
+  {
+    id: 3,
+    name: "Temporary Address",
+    content: () => <Address />,
+    state: "inactive",
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Accordion data={data} />
+    </div>
+  );
+}
+
+export default App;
